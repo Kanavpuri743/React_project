1b115a98dc656ec84dc4da1944146f660696c309
Kanavpuri743 <142196555+Kanavpuri743@users.noreply.github.com>
2025-08-28 13:13:37 +0530

    Update header.js

--- src/header.js
+++ src/header.js
@@ -9,15 +9,7 @@ export default function Header(props) {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    {/*<li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#"></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#"></a>
-            </li>*/}
-           
-            
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">          
           </ul>
           {props.searchbar ? <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
